0fee81f7dd073cd27dadd933a318388c3ca7241e
Ankur1326 <swamiankur46@gmail.com>
2024-01-16 17:30:06 +0530

    add info route

--- navigation/StackNavigation.jsx
+++ navigation/StackNavigation.jsx
@@ -15,6 +15,7 @@ import { FontAwesome5 } from '@expo/vector-icons';
 
 import ProfileScreen from "../screens/ProfileScreen";
 import CartScreen from "../screens/CartScreen";
+import ProductInfoScreen from "../screens/ProductInfoScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -27,22 +28,22 @@ const StackNavigation = () => {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} options={{
           tabBarLabel: "Home",
-          tabBarLabelStyle: ({ focused }) => focused ? {color: "#008E97"} : {color: "black"},
+          tabBarLabelStyle: ({ focused }) => focused ? { color: "#008E97" } : { color: "black" },
           headerShown: false,
           tabBarIcon: ({ focused }) => focused ? (<Ionicons name="home" size={24} color="#008E97" />) : (<Ionicons name="home-outline" size={24} color="black" />),
         }} />
 
         <Tab.Screen name="Profile" component={ProfileScreen} options={{
           tabBarLabel: "Profile",
-          tabBarLabelStyle: ({ focused }) => focused ? {color: "#008E97"} : {color: "black"},
+          tabBarLabelStyle: ({ focused }) => focused ? { color: "#008E97" } : { color: "black" },
           headerShown: false,
           tabBarIcon: ({ focused }) => focused ? (<FontAwesome5 name="user-alt" size={24} color="#008E97" />) : (<FontAwesome5 name="user" size={24} color="black" />),
         }} />
-        
+
 
         <Tab.Screen name="Cart" component={CartScreen} options={{
           tabBarLabel: "Cart",
-          tabBarLabelStyle: ({ focused }) => focused ? {color: "#008E97"} : {color: "black"},
+          tabBarLabelStyle: ({ focused }) => focused ? { color: "#008E97" } : { color: "black" },
           headerShown: false,
           tabBarIcon: ({ focused }) => focused ? (<Ionicons name="cart" size={24} color="#008E97" />) : (<AntDesign name="shoppingcart" size={24} color="black" />),
         }} />
@@ -57,6 +58,7 @@ const StackNavigation = () => {
         <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Main" component={BottomTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="Info" component={ProductInfoScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
