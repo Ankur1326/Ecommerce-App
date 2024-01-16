f0267a7f4fd8b067ade8c3a1887afafd3481ee36
Ankur1326 <swamiankur46@gmail.com>
2024-01-17 00:16:51 +0530

    error resolve

--- redux/CartReducer.js
+++ redux/CartReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const cartSlice = createSlice({
+export const CartSlice = createSlice({
   name: "cart",
   initialState: {
     cart: [],
@@ -10,44 +10,45 @@ export const cartSlice = createSlice({
       const itemPresent = state.cart.find(
         (item) => item.id === action.payload.id
       );
-      if (!itemPresent) {
-        state.cart.push({ ...action.payload, quantity: 1 });
-      } else {
+      if (itemPresent) {
         itemPresent.quantity++;
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
       }
     },
-  },
-  removeFromCart: (state, action) => {
-    state.cart = state.cart.filter((item) => item.id !== action.payload.id);
-  },
-  incrementQuantity: (state, action) => {
-    const itemPresent = state.cart.find(
-      (item) => item.id === action.payload.id
-    );
-    itemPresent.quantity++;
-  },
-  decrementquantity: () => {
-    const itemPresent = state.cart.find(
-      (item) => item.id === action.payload.id
-    );
-    if (itemPresent.quantity === 1) {
-      itemPresent.quantity = 0;
-      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
-    } else {
-      itemPresent.quantity--;
+    removeFromCart: (state, action) => {
+      const removeItem = state.cart.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.cart = removeItem;
+    },
+    incementQuantity: (state, action) => {
+      const itemPresent = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      itemPresent.quantity++;
+    },
+    decrementQuantity: (state, action) => {
+      const itemPresent = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      if (itemPresent.quantity === 1) {
+        itemPresent.quantity = 0;
+        const removeItem = state.cart.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.cart = removeItem;
+      } else {
+        itemPresent.quantity--;
+      }
+    },
+    cleanCart:(state) => {
+        state.cart = [];
     }
-  },
-  cleanCart: (state) => {
-    state.cart = [];
   },
 });
 
-export const {
-  addToCart,
-  removeFromCart,
-  incrementQuantity,
-  decrementquantity,
-  cleanCart,
-} = cartSlice.actions;
 
-export default cartSlice.reducer
+export const {addToCart,removeFromCart,incementQuantity,decrementQuantity,cleanCart} = CartSlice.actions;
+
+export default CartSlice.reducer
