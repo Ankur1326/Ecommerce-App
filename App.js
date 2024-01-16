7827ad5b5e08d614396e389b38744cffd0cf4553
Ankur1326 <swamiankur46@gmail.com>
2024-01-17 00:15:17 +0530

    add provider store

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigation from "./navigation/StackNavigation";
+import { Provider } from "react-redux";
+import store from "./Store";
 
 export default function App() {
   return (
     <>
+      <Provider store={store}>
         <StackNavigation />
+      </Provider>
     </>
   );
 }
