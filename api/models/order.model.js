4b7ac14889eddcdb1cf3202fb484dbbb2fc754e0
Ankur1326 <swamiankur46@gmail.com>
2024-01-21 07:27:48 +0530

    create order.modal

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -1,9 +1,74 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose"
 
-const orderSchema = mongoose.Schema({
-    
-}, {timestamps: true})
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    products: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    shippingAddress: {
+      name: {
+        type: String,
+        required: true,
+      },
+      mobileNo: {
+        type: String,
+        required: true,
+      },
+      houseNo: {
+        type: String,
+        required: true,
+      },
+      street: {
+        type: String,
+        // required: true,
+      },
+      landmark: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
-const Order = mongoose.model("Order", orderSchema);
+export const Order = mongoose.model("Order", orderSchema);
 
-module.exports = Order;
+
